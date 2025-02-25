79c9d02d3ebc18659213d464637a6a23a0756d0e
archannathulasiraman <archanna2004@gmail.com>
2025-02-25 17:26:28 +0530

    Delete UtilityClass.java

--- UtilityClass.java
+++
@@ -1,9 +0,0 @@
-package utility;
-
-public class UtilityClass {
-	 public static int findLength(int number) {
-	        return String.valueOf(Math.abs(number)).length(); // Convert to string and find the length
-	    }
-	
-
-}
